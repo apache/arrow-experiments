6399571a420b3a79c214b8796409d83f4c622c69
Dewey Dunnington <dewey@fishandwhistle.net>
2024-03-06 22:45:13 -0400

    add example data

---
+++ data/arrow-commits/arrow-commits.R
@@ -0,0 +1,35 @@
+
+library(gert)
+library(arrow, warn.conflicts = FALSE)
+
+# Assumes the working directory is
+out_stream <- file.path(getwd(), "data/arrow-commits/arrow-commits.arrows")
+
+# ...and that an apache/arrow checkout at ../arrow
+commits_table <- withr::with_dir("../arrow", {
+  commits <- gert::git_log(max = .Machine$integer.max)
+  commits$time <- lubridate::with_tz(commits$time, "UTC")
+  commits_table <- arrow_table(commits)
+  commits_table$metadata <- NULL
+  commits_table
+})
+
+# R bindings don't expose non-default batch size, so do the chunking manually
+batch_size <- 1024
+num_batches <- nrow(commits_table) %/% batch_size + 1
+batch <- function(i) {
+  begin <- (i - 1) * batch_size + 1
+  end <- min(begin + batch_size - 1, nrow(commits_table))
+  commits_table[begin:end, ]
+}
+
+fs <- LocalFileSystem$create()
+out <- fs$OpenOutputStream(out_stream)
+writer <- RecordBatchStreamWriter$create(out, commits_table$schema)
+for (i in seq_len(num_batches)) {
+  writer$write_table(batch(i))
+}
+writer$close()
+
+# Check a simple read of the file
+stopifnot(identical(read_ipc_stream(out_stream), commits))
