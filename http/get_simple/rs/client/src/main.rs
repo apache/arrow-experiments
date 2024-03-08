76d8c2428d6ca7e7527c8a2801311002172a07af
Matthijs Brobbel <m1brobbel@gmail.com>
2024-03-08 19:19:23 +0100

    Wrap span around the full example

--- http/get_simple/rs/client/src/main.rs
+++ http/get_simple/rs/client/src/main.rs
@@ -29,37 +29,39 @@ fn main() {
         .with_span_events(FmtSpan::CLOSE)
         .init();
 
-    // Connect to server.
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8000);
-    match TcpStream::connect(addr) {
-        Ok(mut stream) => {
-            info_span!("Reading Arrow IPC stream", %addr).in_scope(|| {
-                info!("Connected");
+    info_span!("get_simple").in_scope(|| {
+        // Connect to server.
+        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8000);
+        match TcpStream::connect(addr) {
+            Ok(mut stream) => {
+                info_span!("Reading Arrow IPC stream", %addr).in_scope(|| {
+                    info!("Connected");
 
-                // Send request.
-                stream
-                    .write_all("GET / HTTP/1.0\r\n\r\n".as_bytes())
-                    .unwrap();
+                    // Send request.
+                    stream
+                        .write_all("GET / HTTP/1.0\r\n\r\n".as_bytes())
+                        .unwrap();
 
-                // Ignore response header.
-                for _ in BufReader::new(&mut stream)
-                    .lines()
-                    .take_while(|line| line.as_ref().is_ok_and(|line| !line.is_empty()))
-                {
-                }
+                    // Ignore response header.
+                    for _ in BufReader::new(&mut stream)
+                        .lines()
+                        .take_while(|line| line.as_ref().is_ok_and(|line| !line.is_empty()))
+                    {
+                    }
 
-                // Read Arrow IPC stream
-                let reader = StreamReader::try_new_unbuffered(stream, None).unwrap();
-                let batches = reader.flat_map(Result::ok).collect::<Vec<_>>();
+                    // Read Arrow IPC stream
+                    let reader = StreamReader::try_new(stream, None).unwrap();
+                    let batches = reader.flat_map(Result::ok).collect::<Vec<_>>();
 
-                info!(
-                    batches = batches.len(),
-                    rows = batches.iter().map(|rb| rb.num_rows()).sum::<usize>()
-                );
-            });
+                    info!(
+                        batches = batches.len(),
+                        rows = batches.iter().map(|rb| rb.num_rows()).sum::<usize>()
+                    );
+                });
+            }
+            Err(error) => {
+                error!(%error, "Connection failed")
+            }
         }
-        Err(error) => {
-            error!(%error, "Connection failed")
-        }
-    }
+    })
 }
